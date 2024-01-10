3cf085e8de0687aabca7cd8830f30bad768798ec
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-10 20:13:30 +0200

    Add utility img processing funcs

--- lab.py
+++ lab.py
@@ -34,6 +34,18 @@ def frame_num(path: Path) -> int:
     return int(cv2.VideoCapture(str(path)).get(cv2.CAP_PROP_FRAME_COUNT))
 
 
+def monochrome(frame: numpy.ndarray) -> numpy.ndarray:
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+def threshold(frame: numpy.ndarray) -> numpy.ndarray:
+    return 255 - cv2.threshold(frame, 20, 255, cv2.THRESH_BINARY)[1]
+
+
+def blur(frame: numpy.ndarray) -> numpy.ndarray:
+    return cv2.GaussianBlur(frame, (11, 11), 0)
+
+
 def display_frame(frame: numpy.ndarray):
     frame = cv2.resize(frame, None, fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
     cv2.imshow(TITLE, frame)
@@ -42,6 +54,10 @@ def display_frame(frame: numpy.ndarray):
 
 
 if __name__ == '__main__':
-    print(frame_num((next(get_videos()))))
-    for frame in islice(get_frames(next(get_videos())), None, None, 50):
+    # print(frame_num((list(get_videos())[1])))
+    frames = islice(get_frames(list(get_videos())[1]), 150, None, 50)
+    bg = threshold(monochrome(next(frames)))
+    display_frame(bg)
+    for frame in frames:
+        display_frame(threshold(monochrome(frame)))
         display_frame(frame)
