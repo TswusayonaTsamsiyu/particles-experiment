cb3a57c3f4bc51aea213c6a84e5f4c6a91dd22a0
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-11 16:47:32 +0200

    Automatic image resizing

--- viewing.py
+++ viewing.py
@@ -2,11 +2,15 @@ import cv2
 from numpy import ndarray
 
 TITLE = "Frame"
-RESIZE_FACTOR = 0.4
+SCREEN_HEIGHT = 1080
+
+
+def resize(frame: ndarray) -> ndarray:
+    factor = SCREEN_HEIGHT / frame.shape[0] * 0.8
+    return cv2.resize(frame, None, fx=factor, fy=factor)
 
 
 def display_frame(frame: ndarray, title: str = TITLE) -> None:
-    frame = cv2.resize(frame, None, fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
-    cv2.imshow(title, frame)
+    cv2.imshow(title, resize(frame))
     cv2.waitKey(0)
     cv2.destroyWindow(title)
