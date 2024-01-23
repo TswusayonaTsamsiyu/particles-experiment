0e41ab045d4495a40443752e7a77b5ff5886eec6
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-23 23:17:42 +0200

    Split main code and remove unused code that did not work and improve prints

--- lab.py
+++ lab.py
@@ -5,19 +5,15 @@ from typing import Iterable
 from functools import reduce
 
 import image as img
-from video import Video, Frame
+from video import Video
 from viewing import display_frame
 from fs import get_bg_videos, get_rod_videos
-
-THRESH = 5
 
 BG_FRAME = 3600
 JUMP_FRAMES = 20
 
 BLUR_SIZE = 15
 KSIZE = (BLUR_SIZE, BLUR_SIZE)
-
-WINDOW = 200  # frames
 
 
 # TODO:
@@ -37,6 +33,7 @@ def make_binary(frame: ndarray) -> ndarray:
 def has_tracks(frame: ndarray) -> bool:
     mean, std = cv2.meanStdDev(frame)
     min_, max_ = cv2.minMaxLoc(frame)[:2]
+    print(f"Mean: {mean[0][0]}, STD: {std[0][0]}, Max: {max_}")
     return mean > 0.6 and std > 1 and max_ > 25
 
 
@@ -44,23 +41,26 @@ def get_avg_bg(frames: Iterable[ndarray], window: int) -> ndarray:
     return cv2.divide(window, reduce(cv2.add, map(prepare, frames)))
 
 
-if __name__ == '__main__':
+def analyze_video(video: Video) -> None:
+    frames = video.iter_frames(start=BG_FRAME, jump=JUMP_FRAMES)
+    bg = prepare(next(frames).pixels)
+    display_frame(bg, f"Background frame {BG_FRAME}")
+    for frame in frames:
+        print(f"Processing frame {frame.index}")
+        prepared = prepare(frame.pixels)
+        display_frame(prepared, f"Prepared frame {frame.index}")
+        subtracted = cv2.subtract(prepared, bg)
+        print("Tracks detected" if has_tracks(subtracted) else "No tracks")
+        display_frame(make_binary(subtracted), f"Binary frame {frame.index}")
+
+
+def main() -> None:
     example_path = list(get_bg_videos())[1]
-    print(f"Parsing {example_path}...")
+    print(f"Parsing {example_path.name}...")
     with Video(example_path) as video:
         print(f"Video has {video.frame_num} frames.")
-        frames = video.iter_frames(start=BG_FRAME, jump=JUMP_FRAMES)
-        bg = prepare(next(frames).pixels)
-        # bg = get_avg_bg(iter_frames(video, start=BG_FRAME, stop=BG_FRAME + WINDOW), WINDOW)
-        display_frame(bg, f"Background frame {BG_FRAME}")
-        for frame in frames:
-            prepared = prepare(frame.pixels)
-            subtracted = cv2.subtract(prepared, bg)
-            print(f"Mean, STD: {cv2.meanStdDev(subtracted)}\nMin, Max: {cv2.minMaxLoc(subtracted)[:2]}")
-            # if has_tracks(subtracted):
-            #     print(f"Frame {frame.index} is the new BG")
-            #     bg = prepared
-            #     bg = get_avg_bg(iter_frames(video, start=frame.index - WINDOW, stop=frame.index + WINDOW), WINDOW * 2)
-            print("Tracks detected" if has_tracks(subtracted) else "No tracks")
-            display_frame(prepared, f"Prepared frame {frame.index}")
-            display_frame(make_binary(subtracted), f"Binary frame {frame.index}")
+        analyze_video(video)
+
+
+if __name__ == '__main__':
+    main()
