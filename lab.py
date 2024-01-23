d1b5da77068ea6d81bdeb48f1faf541a782d822b
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-23 13:19:24 +0200

    Optimize video slicing
    Use internal frame-index-setting mechanism instead of islice to avoid reading frames in between

--- lab.py
+++ lab.py
@@ -1,17 +1,16 @@
 import cv2
 import numpy as np
 from numpy import ndarray
-from itertools import islice
 
 import image as img
 from viewing import display_frame
 from fs import get_bg_videos, get_rod_videos
-from parsing import parse_video, iter_frames, frame_num, frame_index
+from parsing import parse_video, iter_frames, frame_num, next_frame_index
 
 THRESH = 5
 
-BG_FRAME = 150
-JUMP_FRAMES = 50
+BG_FRAME = 3600
+JUMP_FRAMES = 200
 
 BLUR_SIZE = 15
 KSIZE = (BLUR_SIZE, BLUR_SIZE)
@@ -40,17 +39,17 @@ if __name__ == '__main__':
     print(f"Parsing {example_path}")
     with parse_video(example_path) as video:
         print(f"Video has {frame_num(video)} frames")
-        frames = islice(iter_frames(video), BG_FRAME, None, JUMP_FRAMES)
+        frames = iter_frames(video, start=BG_FRAME, jump=JUMP_FRAMES)
         bg = (prepare(next(frames)))
         display_frame(bg, "Background")
         for frame in frames:
-            title = f"Frame {frame_index(video)}"
+            title = f"Frame {next_frame_index(video) - 1}"
             prepared = prepare(frame)
             subtracted = cv2.subtract(prepared, bg)
             print(f"Max: {subtracted.max()}, Mean: {subtracted.mean()}, STD: {subtracted.std()}")
             if not has_tracks(subtracted):
-                print(f"Frame {frame_index(video)} is the new BG")
+                print(f"Frame {next_frame_index(video)} is the new BG")
                 bg = prepare(frame)
-            display_frame(subtracted)
-            display_frame(make_binary(subtracted), title)
             display_frame(prepared, title)
+            display_frame(subtracted, title)
+            display_frame(make_binary(subtracted), title)
--- parsing.py
+++ parsing.py
@@ -1,7 +1,7 @@
 import cv2
 from pathlib import Path
 from numpy import ndarray
-from typing import Iterable
+from typing import Generator
 from contextlib import contextmanager
 
 
@@ -14,17 +14,26 @@ def parse_video(path: Path) -> cv2.VideoCapture:
     video.release()
 
 
-def iter_frames(video: cv2.VideoCapture) -> Iterable[ndarray]:
-    while True:
+def iter_frames(video: cv2.VideoCapture,
+                start: int = 0, stop: int = None, jump: int = 1) -> Generator[ndarray, None, None]:
+    jump_to_frame(video, start)
+    index = 0
+    while index < (stop or frame_num(video)):
         success, frame = video.read()
         if not success:
             break
         yield frame
+        index = next_frame_index(video)
+        jump_to_frame(video, index + jump - 1)
 
 
 def frame_num(video: cv2.VideoCapture) -> int:
     return int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 
-def frame_index(video: cv2.VideoCapture) -> int:
+def next_frame_index(video: cv2.VideoCapture) -> int:
     return int(video.get(cv2.CAP_PROP_POS_FRAMES))
+
+
+def jump_to_frame(video: cv2.VideoCapture, index: int) -> None:
+    video.set(cv2.CAP_PROP_POS_FRAMES, index)
