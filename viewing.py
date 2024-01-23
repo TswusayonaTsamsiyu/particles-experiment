0a58fe90ce62fbffd9bbccf00e23a638d0c18d10
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-23 16:58:00 +0200

    Set displayed window location and size

--- viewing.py
+++ viewing.py
@@ -1,16 +1,10 @@
 import cv2
 from numpy import ndarray
 
-TITLE = "Frame"
-SCREEN_HEIGHT = 1080
 
-
-def resize(frame: ndarray) -> ndarray:
-    factor = SCREEN_HEIGHT / frame.shape[0] * 0.8
-    return cv2.resize(frame, None, fx=factor, fy=factor)
-
-
-def display_frame(frame: ndarray, title: str = TITLE) -> None:
-    cv2.imshow(title, resize(frame))
+def display_frame(frame: ndarray, title: str = "Frame") -> None:
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cv2.moveWindow(title, 0, 0)
+    cv2.imshow(title, frame)
     cv2.waitKey(0)
     cv2.destroyWindow(title)
