f0fa48924874c5750a7a6c565a4658abcd36c0f7
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-23 17:51:49 +0200

    Refactor

--- viewing.py
+++ viewing.py
@@ -1,7 +1,10 @@
 import cv2
+from typing import Tuple
 from numpy import ndarray
+from contextlib import contextmanager
 
 ESC = 27
+CLOSE_BTN = -1
 
 SCALE_FACTOR = 0.8
 SCREEN_SIZE = (1920, 1080)
@@ -17,17 +20,27 @@ def resize_frame(frame: ndarray) -> ndarray:
     return cv2.resize(frame, None, fx=factor, fy=factor)
 
 
-def create_window(frame: ndarray, title: str) -> None:
-    height, width = frame.shape
-    cv2.namedWindow(title, cv2.WINDOW_AUTOSIZE)
-    cv2.imshow(title, frame)
-    cv2.resizeWindow(title, width, height)
-    cv2.moveWindow(title, 0, 0)
+@contextmanager
+def show_window(title: str, image: ndarray) -> None:
+    height, width = image.shape
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cv2.imshow(title, image)
+    adjust_window(title, (width, height))
+    yield
+    try:
+        cv2.destroyWindow(title)
+    except cv2.error:
+        pass
+
+
+def adjust_window(title: str,
+                  size: Tuple[int, int],
+                  position: Tuple[int, int] = (0, 0)) -> None:
+    cv2.resizeWindow(title, *size)
+    cv2.moveWindow(title, *position)
 
 
 def display_frame(frame: ndarray, title: str = "Frame") -> None:
-    create_window(resize_frame(frame), title)
-    key_code = cv2.waitKey(0)
-    cv2.destroyWindow(title)
-    if key_code == ESC:
-        exit()
+    with show_window(title, resize_frame(frame)):
+        if cv2.waitKey(0) in [ESC, CLOSE_BTN]:
+            exit()
