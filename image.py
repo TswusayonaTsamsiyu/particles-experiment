b4ec52ed4b58e4407caaba7ea305c80853756917
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-27 10:07:44 +0200

    Make parameter names and types more generic

--- image.py
+++ image.py
@@ -3,49 +3,49 @@ import numpy as np
 from numpy import ndarray
 from typing import Tuple, Sequence
 
-from utils import Color
+from utils import Color, Image
 
 GREEN = Color(0, 255, 0)
 
 
-def monochrome(frame: ndarray) -> ndarray:
-    return cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+def monochrome(image: Image) -> Image:
+    return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 
-def threshold(frame: ndarray, thresh: int = None) -> ndarray:
+def threshold(image: Image, thresh: int = None) -> Image:
     if thresh:
-        return cv.threshold(frame, thresh, 255, cv.THRESH_BINARY)[1]
-    return cv.threshold(frame, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+        return cv.threshold(image, thresh, 255, cv.THRESH_BINARY)[1]
+    return cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
 
 
-def blur(frame: ndarray, ksize: Tuple[int, int]) -> ndarray:
-    return cv.GaussianBlur(frame, ksize, 0)
+def blur(image: Image, ksize: Tuple[int, int]) -> Image:
+    return cv.GaussianBlur(image, ksize, 0)
 
 
-def crop(frame: ndarray, top: int, bottom: int) -> ndarray:
-    return frame[top:-bottom]
+def crop(image: Image, top: int, bottom: int) -> Image:
+    return image[top:-bottom]
 
 
-def adjust_brightness_contrast(frame: ndarray) -> ndarray:
-    p5 = np.percentile(frame, 5)
-    p95 = np.percentile(frame, 95)
+def adjust_brightness_contrast(image: Image) -> Image:
+    p5 = np.percentile(image, 5)
+    p95 = np.percentile(image, 95)
     stretch = 255 / (p95 - p5)
-    return cv.convertScaleAbs(frame, alpha=stretch, beta=-stretch * p5)
+    return cv.convertScaleAbs(image, alpha=stretch, beta=-stretch * p5)
 
 
-def denoise(frame: ndarray) -> ndarray:
+def denoise(image: Image) -> Image:
     # Slow...
-    return cv.fastNlMeansDenoising(frame)
+    return cv.fastNlMeansDenoising(image)
 
 
-def subtract_bg(frame: ndarray, thresh: int) -> ndarray:
+def subtract_bg(image: Image, thresh: int) -> Image:
     # Doesn't work...
-    return cv.createBackgroundSubtractorMOG2(varThreshold=thresh, detectShadows=False).apply(frame)
+    return cv.createBackgroundSubtractorMOG2(varThreshold=thresh, detectShadows=False).apply(image)
 
 
-def draw_contours(frame: ndarray,
+def draw_contours(image: Image,
                   contours: Sequence[ndarray],
                   color: Color = GREEN,
-                  thickness: int = 2) -> ndarray:
-    rgb_copy = cv.cvtColor(frame.copy(), cv.COLOR_GRAY2BGR)
+                  thickness: int = 2) -> Image:
+    rgb_copy = cv.cvtColor(image.copy(), cv.COLOR_GRAY2BGR)
     return cv.drawContours(rgb_copy, contours, -1, color, thickness)
