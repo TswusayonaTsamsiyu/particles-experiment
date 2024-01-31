129d2fd0632fd32280d8401b0be2292f7fb2681c
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-01-31 17:58:50 +0200

    Add track class

---
+++ track.py
@@ -0,0 +1,20 @@
+from typing import MutableSequence
+
+from contours import Contour
+
+
+class Track:
+    def __init__(self, contours: MutableSequence[Contour], start: int):
+        self.contours = contours
+        self.start = start
+        self.end = start
+
+    def relevant_contour(self) -> Contour:
+        index = min(len(self.contours) - 1, 4)
+        return self.contours[index]
+
+    def type(self) -> str:
+        pass
+
+    def append(self, contour: Contour) -> None:
+        self.contours.append(contour)
