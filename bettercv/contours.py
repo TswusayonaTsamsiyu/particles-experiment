c06a24c6a8f8d34b3485316c949b46c9c50e0a0a
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-02-06 10:12:59 +0200

    Rename

--- bettercv/contours.py
+++ bettercv/contours.py
@@ -53,10 +53,10 @@ class Contour:
     def fit_line(self, dist_type: int = cv.DIST_L2, reps: float = 0.01, aeps: float = 0.01):
         return cv.fitLine(self.points, dist_type, 0, reps, aeps)
 
-    def is_close_to(self, other: "Contour", distance: float, precision: int = 5) -> bool:
+    def is_close_to(self, other: "Contour", distance: float, jump: int = 5) -> bool:
         return any(linalg.norm(p1 - p2) < distance
-                   for p1 in self.points[::precision]
-                   for p2 in other.points[::precision])
+                   for p1 in self.points[::jump]
+                   for p2 in other.points[::jump])
 
 
 def join_contours(contours: Sequence[Contour]) -> Contour:
