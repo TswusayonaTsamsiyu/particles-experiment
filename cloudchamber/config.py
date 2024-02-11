0b9caf7d146c17c5013fb8339bd5ddacff753b72
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-02-11 17:20:27 +0200

    Make editable config

---
+++ cloudchamber/config.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    # Preprocessing
+    blur_size: int = 15
+    # Tracking
+    drift_distance: int = 40
+    # Joining
+    dist_close: int = 100
+    # Filtering
+    min_contour_size: int = 500
+    min_track_length: int = 5
+    min_threshold: int = 1
+    # BG computation
+    bg_jump: int = 5
+    bg_batch_size: int = 200
+    # Resizing
+    scale_factor: float = 0.6
+
+    @classmethod
+    def merge(cls, config: "Dict") -> "Config":
+        return cls(**config)
--- cloudchamber/detection.py
+++ cloudchamber/detection.py
@@ -6,43 +6,25 @@ from bettercv.track import Track
 from bettercv.video import Frame
 from bettercv.contours import Contour, find_contours, join_close_contours
 
+from .config import Config
 from .particle import ParticleEvent
 
-# Preprocessing
-BLUR_SIZE = 15
-KSIZE = (BLUR_SIZE, BLUR_SIZE)
 
-# Tracking
-DRIFT_DISTANCE = 40
-
-# Joining
-DIST_CLOSE = 100
-
-# Filtering
-MIN_CONTOUR_SIZE = 500
-MIN_TRACK_LENGTH = 5
-MIN_THRESHOLD = 1
-
-# BG computation
-BG_JUMP = 5
-BG_BATCH_SIZE = 200
-
-# Resizing
-SCALE_FACTOR = 0.6
+def preprocess(frame: Frame, config: Config) -> Frame:
+    return frame.with_image(img.blur(
+        img.grayscale(img.scale(frame.image, config.scale_factor)),
+        (config.blur_size, config.blur_size)
+    ))
 
 
-def preprocess(frame: Frame) -> Frame:
-    return frame.with_image(img.blur(img.grayscale(img.scale(frame.image, SCALE_FACTOR)), KSIZE))
+def has_tracks(threshold: float, min_thresh: float) -> bool:
+    return threshold > min_thresh
 
 
-def has_tracks(threshold: float) -> bool:
-    return threshold > MIN_THRESHOLD
-
-
-def find_prominent_contours(binary: Frame) -> Sequence[Contour]:
+def find_prominent_contours(binary: Frame, min_size: int) -> Sequence[Contour]:
     return tuple(contour
                  for contour in find_contours(binary.image, external_only=True)
-                 if contour.area > MIN_CONTOUR_SIZE)
+                 if contour.area > min_size)
 
 
 def retain_track_like(contours: Iterable[Contour]) -> Iterable[Contour]:
@@ -51,17 +33,18 @@ def retain_track_like(contours: Iterable[Contour]) -> Iterable[Contour]:
             and (contour.width < 100))
 
 
-def find_close_tracks(contour: Contour, index: int, tracks: Iterable[Track]) -> List[Track]:
+def find_close_tracks(contour: Contour, index: int, tracks: Iterable[Track], drift_distance: int) -> List[Track]:
     return list(track for track in tracks
-                if (track.end.contour.centroid.distance_to(contour.centroid) < DRIFT_DISTANCE)
+                if (track.end.contour.centroid.distance_to(contour.centroid) < drift_distance)
                 and (index - track.end.frame.index == 1))
 
 
 def update_tracks(tracks: MutableSequence[Track],
                   contours: Iterable[Contour],
-                  binary: Frame) -> None:
+                  binary: Frame,
+                  config: Config) -> None:
     for contour in contours:
-        close = find_close_tracks(contour, binary.index, tracks)
+        close = find_close_tracks(contour, binary.index, tracks, config.drift_distance)
         if len(close) > 1:
             raise Exception("Multiple tracks detected for same contour!")
         if len(close) == 1:
@@ -72,27 +55,29 @@ def update_tracks(tracks: MutableSequence[Track],
             tracks.append(new_track)
 
 
-def subtract_bg(frames: Iterable[Frame]) -> Generator[Frame, None, None]:
-    for batch in chunked_even(frames, BG_BATCH_SIZE):
+def subtract_bg(frames: Iterable[Frame], config: Config) -> Generator[Frame, None, None]:
+    for batch in chunked_even(frames, config.bg_batch_size):
         print(f"Computing BG for {batch[0].index}-{batch[-1].index}")
-        bg = img.avg(frame.image for frame in batch[::BG_JUMP])
+        bg = img.avg(frame.image for frame in batch[::config.bg_jump])
         # disp.show([disp.fit_to_screen(disp.Window(bg, "Avg BG"))])
         for frame in batch:
             yield frame.with_image(img.subtract(frame.image, bg))
 
 
-def binaries_with_tracks(frames: Iterable[Frame]) -> Generator[Frame, None, None]:
+def binaries_with_tracks(frames: Iterable[Frame], config: Config) -> Generator[Frame, None, None]:
     for frame in frames:
         thresh, binary = img.threshold_otsu(frame.image)
-        if has_tracks(thresh):
+        if has_tracks(thresh, config.min_threshold):
             yield frame.with_image(binary)
 
 
-def detect_tracks(frames: Iterable[Frame]) -> List[ParticleEvent]:
+def detect_tracks(frames: Iterable[Frame], **config) -> List[ParticleEvent]:
     tracks: List[Track] = []
-    for binary in binaries_with_tracks(subtract_bg(map(preprocess, frames))):
-        contours = retain_track_like(join_close_contours(find_prominent_contours(binary), DIST_CLOSE))
-        update_tracks(tracks, contours, binary)
+    config = Config.merge(config)
+    for binary in binaries_with_tracks(subtract_bg((preprocess(frame, config) for frame in frames), config), config):
+        contours = retain_track_like(
+            join_close_contours(find_prominent_contours(binary, config.min_contour_size), config.dist_close))
+        update_tracks(tracks, contours, binary, config)
     return [ParticleEvent(track)
             for track in tracks
-            if track.extent > MIN_TRACK_LENGTH]
+            if track.extent > config.min_track_length]
