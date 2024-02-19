b3c25b02dcff9f97d908e08c097223e59e0f1eee
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-02-19 23:29:26 +0200

    Fix display from CSV

--- fs.py
+++ fs.py
@@ -1,10 +1,17 @@
+import json
+import numpy as np
 import pandas as pd
 from pathlib import Path
+from contextlib import ExitStack
+from collections import namedtuple
 from typing import List, Tuple, Iterable
 
+from bettercv.video import Video
 from bettercv.track import Snapshot
 from bettercv.contours import Contour
 
+from cloudchamber.config import Config
+from cloudchamber.detection import preprocess
 from cloudchamber.particle import ParticleTrack
 
 from root import ROOT_PATH
@@ -15,8 +22,8 @@ ROD_RADIATION_PATH = ROOT_PATH / "Rod"
 CSV_PATH = ROOT_PATH / "csv"
 
 _COLUMNS = ("Width", "Length", "Angle",
-            "Start Index", "End Index",
-            "Snapshot Index", "Contour",
+            "Start", "End",
+            "SnapshotIndex", "Contour",
             "Video")
 
 
@@ -40,21 +47,37 @@ def get_csvs() -> List[Path]:
     return [path for path in CSV_PATH.iterdir() if path.suffix.lower() == ".csv"]
 
 
-def _csv_contour(contour: Contour) -> str:
-    return ":".join(",".join(map(str, point[0])) for point in contour.points)
+def _serialize_contour(contour: Contour) -> str:
+    return json.dumps(contour.points.tolist())
 
 
-def _csv_row(particle: ParticleTrack) -> Tuple:
+def _parse_contour(points: str) -> Contour:
+    return Contour(np.array(json.loads(points), dtype=np.int32))
+
+
+def _serialize_particle(particle: ParticleTrack) -> Tuple:
     return (particle.width, particle.length, particle.angle,
             particle.start, particle.end,
-            particle.snapshot.frame.index, _csv_contour(particle.snapshot.contour),
+            particle.snapshot.frame.index, _serialize_contour(particle.snapshot.contour),
             particle.snapshot.frame.video)
 
 
+def _parse_particle(row: namedtuple, video: Video) -> ParticleTrack:
+    return ParticleTrack(
+        (row.Start, row.End),
+        Snapshot(
+            preprocess(video[row.SnapshotIndex], Config),
+            _parse_contour(row.Contour)
+        )
+    )
+
+
 def save_particles(particles: Iterable[ParticleTrack], path: Path) -> None:
-    pd.DataFrame(map(_csv_row, particles), columns=_COLUMNS).to_csv(path, index=False)
+    pd.DataFrame(map(_serialize_particle, particles), columns=_COLUMNS).to_csv(path, index=False)
 
 
 def read_particles(path: Path) -> List[ParticleTrack]:
-    return [ParticleTrack((row[3], row[4]), Snapshot(row[5], Contour(row[6])))
-            for row in pd.read_csv(path).iterrows()]
+    df = pd.read_csv(path)
+    with ExitStack() as stack:
+        videos = {path: stack.enter_context(Video(path)) for path in df.Video.unique()}
+        return [_parse_particle(row, videos[row.Video]) for row in df.itertuples()]
