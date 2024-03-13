7b64a85c4ceea7f1bd2cdaf41ef1071ffa3b2afa
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-03-13 16:46:04 +0200

    Improve config

--- cloudchamber/config.py
+++ cloudchamber/config.py
@@ -7,13 +7,13 @@ class Config:
     # Preprocessing
     blur_size: int = 15
     # Tracking
-    drift_distance: int = 40
+    drift_distance: int = 120
     # Joining
-    dist_close: int = 100
+    dist_close: int = 30
     # Filtering
     min_contour_size: int = 500
     min_track_length: int = 5
-    min_threshold: int = 1
+    min_threshold: int = 3
     # BG computation
     bg_method: str = "avg"  # or "replace"
     bg_jump: int = 5
