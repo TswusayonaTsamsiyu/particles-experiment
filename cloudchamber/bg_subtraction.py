fc5ff5591f3d96153116d69a1679e6e69fb897a1
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-03-15 00:04:48 +0200

    Add some experimental functions

--- cloudchamber/bg_subtraction.py
+++ cloudchamber/bg_subtraction.py
@@ -13,11 +13,27 @@ def has_tracks(threshold: float, min_thresh: float) -> bool:
     return threshold >= min_thresh
 
 
+def has_tracks_2(frame: Frame) -> bool:
+    return img.mean(frame.image)[0] > 0.5
+
+
+def print_mean(frame: Frame) -> Frame:
+    mean = img.mean(frame.image)
+    print(f"{frame.ref.index}: {mean}")
+    return frame
+
+
 def binaries_with_tracks(frames: Iterable[Frame], config: Config) -> Generator[Frame, None, None]:
     for frame in frames:
         thresh, binary = img.threshold_otsu(frame.image)
         if has_tracks(thresh, config.min_threshold):
             yield frame.with_image(binary)
+
+
+def binaries_with_tracks_2(frames: Iterable[Frame], config: Config) -> Generator[Frame, None, None]:
+    for frame in frames:
+        if has_tracks_2(frame):
+            yield frame.with_image(img.threshold_binary(frame.image, 1))
 
 
 def subtract_bg_avg(frames: Iterable[Frame], config: Config) -> Generator[Frame, None, None]:
